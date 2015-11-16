13872e0697f32df13558b6acfa48541e5ff77825
josetorres88l <josetorres88l.et@gmail.com>
2015-11-16 16:40:14 -0430

    segundo commit

---
+++ portal3/portal3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace portal3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Negocio()
+        {
+            ViewBag.Message = "Your config page.";
+
+            return View();
+        }
+        public ActionResult Acerca()
+        {
+            ViewBag.Message = "Your config page.";
+
+            return View();
+        }
+        public ActionResult Contacto()
+        {
+            ViewBag.Message = "Your config page.";
+
+            return View();
+        }
+    }
+}
---
+++ portal3/portal3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(portal3.Startup))]
+namespace portal3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
